55b64265dd71b331f4b3352fd3dfc2d53bb28a33
Humberto BM Junior <64866505+jrhumberto@users.noreply.github.com>
2021-11-08 04:53:32 -0300

    Create pdf.py

---
+++ src/pdf.py
@@ -0,0 +1,15 @@
+#
+#
+#
+result_list = []
+reader = PyPDF2.PdfFileReader(file)
+for page_number in range(0, reader.numPages):
+     page = reader.getPage(page_number)
+     page_content = page.extractText()
+     if search_term in page_content:
+          result = {
+               "page": page_number,
+               "content": page_content
+          }
+          result_list.append(result)
+reader.close()
